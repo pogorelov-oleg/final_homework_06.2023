1e35ac5ab1395fb0422a8c8f1eb428491a6cfe03
pogorelov <89607888555@mail.ru>
2023-06-09 17:03:21 +0700

    created method GetUserArrayString

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@ PrintArray(arrString);
 Console.Write(" -> ");
 PrintArray(arrNewSrting);
 
+string[] GetArrayString(int arrLength)
+{
+    string[] array = new string[arrLength];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите значение {i + 1} ячейки");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
 void PrintArray(string[] array)
 {
     Console.Write($"[{String.Join(", ", array)}]");
